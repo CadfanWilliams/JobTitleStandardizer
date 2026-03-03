7861f52cc931c8ce2df8d0c3e5cac16feac7800f
Cadfan <cadfanw@gmail.com>
2026-03-03 16:14:08 +0000

    Create JobTitleComparator

---
+++ src/normaliser/JobTitleComparator.java
@@ -0,0 +1,9 @@
+package normaliser;
+
+public class JobTitleComparator implements Comparator{
+    // temp comparator to start put project together so I can start running it
+    @Override
+    public double Compare(String input, String target){
+        return 0;
+    }
+}
