21f47df58634030f4ad3bca8434bb98e4b73a230
Cadfan <cadfanw@gmail.com>
2026-03-03 13:22:16 +0000

    Create Normaliser class and create contstructor

--- src/Normaliser.java
+++ src/Normaliser.java
@@ -1,8 +1,22 @@
+import java.util.List;
+
 public class Normaliser {
+    private final List<String> JobTitlesList;
+    private final List<String> NormalisedJobTitlesList;
+
+    Normaliser(List<String> jobTitles, List<String> normalisedJobTitles) {
+        this.JobTitlesList = jobTitles;
+        this.NormalisedJobTitlesList = normalisedJobTitles;
+    }
+
+    int compare(String jobTitle, String normalisedJobTitle) {
+        // This function will compare a job title against a normalised job titles and return a quality
+        return 0;
+    }
+
     //take in job titles
-    //compare each job title in list against normalized job titles list
-    //for each job title give a score of "closeness" against each normalized job title
+    //compare each job title in list against normalised job titles list
+    //for each job title give a score of "closeness" against each normalised job title
     //highest score per job title is what it gets assigned to..
     //output "Job title 'C# Engineer' was normalised to 'Software Engineer'"
-
 }
